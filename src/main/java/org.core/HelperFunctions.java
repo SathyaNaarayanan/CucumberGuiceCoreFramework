39e088edfaae48b90d3aa506ef8852a811e42136
Sathya <sathyas0811@gmail.com>
2026-04-29 23:34:07 +0800

    Version 1.0.2

--- src/main/java/org.core/HelperFunctions.java
+++ src/main/java/org.core/HelperFunctions.java
@@ -8,14 +8,16 @@ import org.openqa.selenium.support.ui.Select;
 
 public class HelperFunctions {
 
+    @Inject
+    BaseDriver driver;
 
     public void dropDownSelect(WebElement e, String value){
         new Select(e).selectByVisibleText(value);
     }
 
 
-    public void scrollTOE(BaseDriver driver, WebElement e){
-        Actions act = new Actions((WebDriver) driver);
+    public void scrollTOE(WebElement e){
+        Actions act = new Actions(driver.getDriver());
         act.scrollToElement(e).build().perform();
     }
     public void sendKeys(WebElement ele, String value) {
